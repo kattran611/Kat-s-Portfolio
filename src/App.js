2689028f411994e0340222ae9a0f6744d20fd2a5
Katrina Tran <kat.c.tran@gmail.com>
2018-12-18 19:58:48 -0700

    background

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 // import './App.css';
 // import Hello from './components/Hello';
 import NavBar from './components/NavBar';
+import {WelcomeBackground} from './components/Background'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar />
-        <p>Hi, my name is Katrina! I'm a full stack web developer</p>
+        <WelcomeBackground>
+        <p>Hi, my name is Katrina! I'm a full stack web developer.</p>
+      </WelcomeBackground>
       </div>
     );
   }
