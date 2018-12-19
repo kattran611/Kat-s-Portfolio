592520c239f2dc70f11849b343f221c86fa591c0
Katrina Tran <kat.c.tran@gmail.com>
2018-12-18 19:39:44 -0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
-import Hello from './components/Hello';
+// import './App.css';
+// import Hello from './components/Hello';
+import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <p>Hi, my name is Katrina! I'm a full stack web developer</p>
+        <NavBar />
+        <p>Hi, my name is Katrina! I'm a full stack web developer</p>
       </div>
     );
   }
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
-import Background from '../images/what-the-hex-dark.png';
-
-const Hello = {
-  backgroundImage: `url(${Background});`
-};
-
-
-export default Hello;
+// import React, {Component} from 'react';
+// import Background from '../Images/what-the-hex-dark.png';
+//
+// const Hello = {
+//   backgroundImage: `url(${Background});`,
+//
+//
+//
+// export default Hello;
