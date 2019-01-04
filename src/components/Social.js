af50322e8a8bf82cdbd828143d5bfa6d5e4e4a1b
Katrina Tran <kat.c.tran@gmail.com>
2019-01-03 22:27:44 -0700

    linked social

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,13 +3,13 @@ import { Button, Icon } from 'semantic-ui-react'
 
 const SocialButton = () => (
   <div>
-    <Button color='facebook'>
+    <Button color='facebook' href='https://www.facebook.com/kat.c.tran'>
       <Icon name='facebook' /> Facebook
     </Button>
-    <Button color='linkedin'>
+    <Button color='linkedin' href='https://www.linkedin.com/in/katrinactran/'>
       <Icon name='linkedin' /> LinkedIn
     </Button>
-    <Button color='instagram'>
+    <Button color='instagram' href='https://www.instagram.com/_kattran/?hl=en'>
       <Icon name='instagram' /> Instagram
     </Button>
   </div>
