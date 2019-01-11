0ddbc13fd98b6764fbfc13e75a8770d1f3b2cd98
Katrina Tran <kat.c.tran@gmail.com>
2019-01-10 23:26:21 -0700

    changes

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,7 +3,7 @@ import { Image, Segment } from 'semantic-ui-react'
 import cabo from '../IMG/cabo.jpg'
 
 
-const PersonalImage = () => <Image centerd src={cabo} size='large' />
+const PersonalImage = () => <Image centerd src={cabo} size='medium' />
 
 
 export default PersonalImage;
--- src/components/Social.js
+++ src/components/Social.js
@@ -18,4 +18,6 @@ const SocialButton = () => (
  </span>
 )
 
+
+
 export default SocialButton;
