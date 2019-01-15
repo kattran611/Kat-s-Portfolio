3baac8d42a25729a9a21773235df0bf56a63a951
Katrina Tran <kat.c.tran@gmail.com>
2019-01-14 23:10:55 -0700

    added icon

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,22 +3,25 @@ import { Button, Icon } from 'semantic-ui-react'
 
 const SocialButton = () => (
   <span>
-	 <Icon name='mail'/> kat.c.tran@gmail.com
+	 {/* <Icon name='mail'/> kat.c.tran@gmail.com
     <Button color='facebook' target='_blank' href='https://www.facebook.com/kat.c.tran'>
       <Icon name='facebook' /> Facebook
     </Button>
     <Button color='linkedin' target='_blank' href='https://www.linkedin.com/in/katrinactran/'>
-      <Icon name='linkedin' /> LinkedIn
+
+	 <Icon name='linkedin' /> LinkedIn
     </Button>
     <Button color='instagram' target='_blank' href='https://www.instagram.com/_kattran/?hl=en'>
-      <Icon name='instagram' /> Instagram
-    </Button>
-	 <Button color='GitHub' target='_blank' href='https://github.com/kattran611'>
-		 <Icon name='GitHub' /> GitHub
-	 </Button>
+
+	<Icon name='instagram' /> Instagram
+	 </Button> */}
+	 {/* <Button color='GitHub' target='_blank' href='https://github.com/kattran611'> */}
+
+	<Icon name = "github"
+		onClick = "http://github.com/kattran611"
+		size = 'huge'
+	/>
+
  </span>
 )
-
-
-
 export default SocialButton;
