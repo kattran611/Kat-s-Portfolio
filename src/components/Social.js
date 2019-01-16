57460af0bd890df4c7e2aaaa7144986dc21d1488
Katrina Tran <kat.c.tran@gmail.com>
2019-01-16 00:01:21 -0700

    added email

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,25 +3,32 @@ import { Icon } from 'semantic-ui-react'
 
 const SocialIcon = () => (
   <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
-	<a href = "https://github.com/kattran611"  centered target="_blank" rel="noopener noreferrer">
+
+	<a href="mailto:kat.c.tran@gmail.com" target="_blank" rel="noopener noreferrer">
+		<Icon name = "mail"
+			size = "huge"
+			link
+		/>
+	</a>
+	<a href = "https://github.com/kattran611" target="_blank" rel="noopener noreferrer">
 		<Icon name = "github"
 			size = 'huge'
 			link
 		/>
 	</a>
-	<a href = "https://www.facebook.com/kat.c.tran" centered target="_blank" rel="noopener noreferrer">
+	<a href = "https://www.facebook.com/kat.c.tran" target="_blank" rel="noopener noreferrer">
 		<Icon name = "facebook"
 			size = 'huge'
 			link
 		/>
 	</a>
-	<a href= "https://www.linkedin.com/in/katrinactran/" centered target="_blank" rel="noopener noreferrer">
+	<a href= "https://www.linkedin.com/in/katrinactran/" target="_blank" rel="noopener noreferrer">
 		<Icon name = "linkedin"
 			size = "huge"
 			link
 		/>
 	</a>
-	<a href= "https://www.instagram.com/_kattran/?hl=en" centered target="_blank" rel="noopener noreferrer">
+	<a href= "https://www.instagram.com/_kattran/?hl=en" target="_blank" rel="noopener noreferrer">
 		<Icon name = "instagram"
 			size = "huge"
 			link
