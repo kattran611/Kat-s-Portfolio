8682381fbd18b5654a5adb3f58817dcfe4f53365
Katrina Tran <kat.c.tran@gmail.com>
2019-01-22 22:31:41 -0700

    cleaning up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-// import './App.css';
-// import Hello from './components/Hello';
 import NavBar from './components/NavBar';
 import {WelcomeBackground} from './components/Background';
 import Header from './StyledComponents/Header';
--- src/components/Hello.js
+++
@@ -1,9 +0,0 @@
-// import React, {Component} from 'react';
-// import Background from '../Images/what-the-hex-dark.png';
-//
-// const Hello = {
-//   backgroundImage: `url(${Background});`,
-//
-//
-//
-// export default Hello;
